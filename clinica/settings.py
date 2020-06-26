114e52186178c71758a2783b25833019350b0195
Bruno-Duarte <fbruno.duarte@outlook.com>
2020-06-26 10:44:53 -0300

    DEBUG FALSE

--- clinica/settings.py
+++ clinica/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dd_@ms^_fmn#t=qpdjryey5x*hrlkz97jsckx)n$(!4n7s5m=*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
