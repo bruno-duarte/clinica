a43de0695e7b6bc11960429988f3b8fb77ed9b08
Bruno-Duarte <fbruno.duarte@outlook.com>
2020-06-27 12:14:55 -0300

    Remoção de pastas desnecessárias

--- lib/python3.8/site-packages/datetimepicker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from .views import JSTemplateView
-
-
-urlpatterns = [url(r'^loader.js',
-               JSTemplateView.as_view(),
-               name='datetimepicker-loader')]
