260d57013a6286ae40b7d8ea7461eb4c1ce04a94
Bruno-Duarte <fbruno.duarte@outlook.com>
2020-10-17 10:38:51 -0300

    Arquivos do Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn clinica.wsgi --log-file -
--- clinica/settings.py
+++ clinica/settings.py
@@ -74,27 +74,26 @@ WSGI_APPLICATION = 'clinica.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
 '''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'clinica',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '1245',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
-'''
+
 
 '''
 DATABASES = {
